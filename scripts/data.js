1b15df21bb9252a2dddc658615c77bb771abdb38
Philip Turkiewicz <philip.turkiewicz@live.ca>
2020-08-26 10:16:51 -0400

    added modules, adjusted comments

---
+++ scripts/data.js
@@ -0,0 +1,77 @@
+const dragData = {};
+
+// create two objects, 1 for drag first names + 1 for drag last names 
+dragData.firstNames = {
+   a: ['amber', 'anita', 'alicia', 'arizona'],
+   b: ['barbie', 'betty', 'baby', 'bon bon', 'brittney'],
+   c: ['cherry', 'celine', 'cher', 'carol', 'crystal'],
+   d: ['diamond', 'danny', 'destiny', 'deborah'],
+   e: ['edna', 'elton', 'elsa', 'egot'],
+   f: ['foxy', 'fetta', 'fishy', 'fortuna', 'finessa'],
+   g: ['goodie', 'gilda', 'gretta', 'gia'],
+   h: ['harmony', 'hilda', 'hippy', 'humphrey', 'harley'],
+   i: ['ida', 'ivana', 'ivanka', 'iridescent'],
+   j: ['jasmine', 'jill', 'jam', 'jay', 'juicy'],
+   k: ['kandi', 'katy', 'kimmy', 'kimchi', 'kristi', 'krusti', 'karen'],
+   l: ['lady', 'lil', 'lula', 'lusty'],
+   m: ['mistress', 'miss', 'mariah', 'melon', 'manny'],
+   n: ['naomi', 'nelly', 'nora', 'nina', 'neNe', 'nip'],
+   o: ['ornatia', 'olive', 'okra', 'olivia'],
+   p: ['portia', 'pearl', 'pissy', 'penny', 'pepper'],
+   q: ['queen', 'queer', 'quest', 'quincy'],
+   r: ['rosalind', 'radisha', 'raspy', 'risky'],
+   s: ['sapphire', 'spamela', 'sessy', 'sissy', 'safette'],
+   t: ['tiffany', 'tina', 'tequila', 'TiTi'],
+   u: ['unique', 'uma', 'uppety', 'uta'],
+   v: ['venus', 'vladamir', 'veronica', 'vicky'],
+   w: ['wynona', 'wisty', 'wiggles', 'whorrid'],
+   x: ['xena', 'xtra', 'xtasy', 'XXX'],
+   y: ['yvie', 'yummy', 'yaYa', 'yessa'],
+   z: ['zenheya', 'zoey', 'ZeZe', 'zoom']
+}
+
+dragData.lastNames = {
+   a: ['aphrodyte', 'arrogance', 'arden', 'anderson'],
+   b: ['boop', 'bellhop', 'bottom', 'belle', 'berry', 'barabasol'],
+   c: ['chardonney', 'cutery', 'crews', 'chanel', 'child'],
+   d: ['danger', 'dijon', 'dairy', 'degenerate', 'de PooPoo', 'destiny'],
+   e: ['essex', 'elliot', 'extra', 'earl jones', 'ecma'],
+   f: ['ferocious', 'fierce', 'fig newton john', 'flora', 'fauna', 'frocker'],
+   g: ['gumdrops', 'gomez', 'grande', 'garbo', 'gingham'],
+   h: ['hart', 'hug\'n\'Kiss', 'hathaway', 'hyak', 'herpes', 'houston', 'heathers'],
+   i: ['illy', 'imp', 'init', 'indigo'],
+   j: ['jeans', 'jovi', 'jones', 'justice', 'juice'],
+   k: ['kane', 'kang', 'krabs', 'kardashian'],
+   l: ['luxury', 'lawless', 'louboutin', 'lumber'],
+   m: ['mystery', 'morose', 'muumuu', 'marbles', 'marriott hotel', 'mcNugget'],
+   n: ['nasty', 'noise', 'nipple', 'norene'],
+   o: ['opalence', 'ovah', 'original', 'opposite', 'ore'],
+   p: ['paris', 'pop', 'pistachio', 'pisser', 'pump', 'pickle', 'peas'],
+   q: ['quinn', 'quick', 'question mark', 'query'],
+   r: ['redd', 'ryan', 'radler', 'roast beef', 'rasputin', 'RESTylane'],
+   s: ['sublime', 'steak', 's\'moore', 'saucy', 'simpson', 'seductive', 'snack'],
+   t: ['trash', 'tuck', 'taquito', 'turnip', 'turnblatt', 'turner', 'tushy', 'tornado', 'top'],
+   u: ['umbridge', 'untouchable', 'upset', 'uwU'],
+   v: ['vanity', 'virgin', 'viper', 'vivacious', 'vibes'],
+   w: ['winter', 'williams', 'werk', 'west', 'whiskers', 'wild', 'wispy'],
+   x: ['xenobia', 'x change', 'x', 'xylophone', 'xtravaganza', 'XOXO'],
+   y: ['yaya', 'yeastwood', 'yummy', 'yogurt', 'yikes'],
+   z: ['zara', 'zest', 'zucker', '\'zine', 'zuckerberg']
+}
+
+dragData.house = [
+   'Snack',
+   'The Drag Queen',
+   'Davenport',
+   'Velour',
+   'Dolezal',
+   'Edwards',
+   'O\'Hara',
+   'Haunt',
+   'Filth',
+   'Xtravaganza',
+   'Astrology',
+   'LaBeija'
+]
+
+export default dragData;
--- scripts/script.js
+++ scripts/script.js
@@ -1,79 +1,7 @@
+import dragData from './data.js'
+
 // Drag Persona Generator App
 const dragApp = {}
-
-// create two objects, 1 for drag first names + 1 for drag last names 
-dragApp.firstNames = {
-   a: ['amber', 'anita', 'alicia', 'arizona'],
-   b: ['barbie', 'betty', 'baby', 'bon bon', 'brittney'],
-   c: ['cherry', 'celine', 'cher', 'carol', 'crystal'],
-   d: ['diamond', 'danny', 'destiny', 'deborah'],
-   e: ['edna', 'elton', 'elsa', 'egot'],
-   f: ['foxy', 'fetta', 'fishy', 'fortuna', 'finessa'],
-   g: ['goodie', 'gilda', 'gretta', 'gia'],
-   h: ['harmony', 'hilda', 'hippy', 'humphrey', 'harley'],
-   i: ['ida', 'ivana', 'ivanka', 'iridescent'],
-   j: ['jasmine', 'jill', 'jam', 'jay', 'juicy'],
-   k: ['kandi', 'katy', 'kimmy', 'kimchi', 'kristi', 'krusti', 'karen'],
-   l: ['lady', 'lil', 'lula', 'lusty'],
-   m: ['mistress', 'miss', 'mariah', 'melon','manny'],
-   n: ['naomi', 'nelly', 'nora', 'nina', 'neNe', 'nip'],
-   o: ['ornatia', 'olive', 'okra', 'olivia'],
-   p: ['portia', 'pearl', 'pissy', 'penny', 'pepper'],
-   q: ['queen', 'queer', 'quest', 'quincy'],
-   r: ['rosalind', 'radisha', 'raspy', 'risky'],
-   s: ['sapphire', 'spamela', 'sessy', 'sissy'],
-   t: ['tiffany', 'tina', 'tequila', 'TiTi'],
-   u: ['unique', 'uma', 'uppety', 'uta'],
-   v: ['venus', 'vladamir', 'veronica', 'vicky'],
-   w: ['wynona', 'wisty', 'wiggles', 'whorrid'],
-   x: ['xena', 'xtra', 'xtasy', 'XXX'],
-   y: ['yvie', 'yummy', 'yaYa', 'yessa'],
-   z: ['zenheya', 'zoey', 'ZeZe', 'zoom']
-}
-
-dragApp.lastNames = {
-   a: ['aphrodyte', 'arrogance', 'arden', 'anderson'],
-   b: ['boop', 'bellhop', 'bottom', 'belle', 'berry', 'barabasol'],
-   c: ['chardonney', 'cutery', 'crews', 'chanel', 'child'],
-   d: ['danger', 'dijon', 'dairy', 'degenerate', 'de PooPoo', 'destiny'],
-   e: ['essex', 'elliot', 'extra', 'earl jones', 'ecma'],
-   f: ['ferocious', 'fierce', 'fig newton john', 'flora', 'fauna', 'frocker'],
-   g: ['gumdrops', 'gomez', 'grande', 'garbo', 'gingham'],
-   h: ['hart', 'hug\'n\'Kiss', 'hathaway', 'hyak', 'herpes', 'houston', 'heathers'],
-   i: ['illy', 'imp', 'init', 'indigo'],
-   j: ['jeans', 'jovi', 'jones', 'justice', 'juice'],
-   k: ['kane', 'kang', 'krabs', 'kardashian'],
-   l: ['luxury', 'lawless', 'louboutin', 'lumber'],
-   m: ['mystery', 'morose', 'muumuu', 'marbles', 'marriott hotel', 'mcNugget'],
-   n: ['nasty', 'noise', 'nipple', 'norene'],
-   o: ['opalence', 'ovah', 'original', 'opposite', 'ore'],
-   p: ['paris', 'pop', 'pistachio', 'pisser', 'pump', 'pickle', 'peas'],
-   q: ['quinn', 'quick', 'question mark', 'query'],
-   r: ['redd', 'ryan', 'radler', 'roast beef', 'rasputin', 'RESTylane'],
-   s: ['sublime', 'steak', 's\'moore', 'saucy', 'simpson', 'seductive', 'snack'],
-   t: ['trash', 'tuck', 'taquito', 'turnip', 'turnblatt', 'turner', 'tushy', 'tornado', 'top'],
-   u: ['umbridge', 'untouchable', 'upset', 'uwU'],
-   v: ['vanity', 'virgin', 'viper', 'vivacious', 'vibes'],
-   w: ['winter', 'williams', 'werk', 'west', 'whiskers', 'wild', 'wispy'],
-   x: ['xenobia', 'x change', 'x', 'xylophone', 'xtravaganza', 'XOXO'],
-   y: ['yaya', 'yeastwood', 'yummy', 'yogurt', 'yikes'],
-   z: ['zara', 'zest', 'zucker', '\'zine', 'zuckerberg']
-}
-
-dragApp.house = [
-   'Snack',
-   'The Drag Queen',
-   'Davenport',
-   'Velour',
-   'Dolezal',
-   'Edwards',
-   'O\'Hara',
-   'Haunt',
-   'Filth',
-   'Xtravaganza',
-   'Astrology',
-   'LaBeija'
-]
 
 // helper functions
 dragApp.randomIndex = (array) => {
@@ -130,8 +58,8 @@ dragApp.errorCatch = (userFirst, userLast) => {
    </div>
    `
    // the display function only goes through if the initials of both inputs matches one of the keys in their respective objects, otherwise it spits out the error message
-   if (dragApp.firstNames[userFirst] && dragApp.lastNames[userLast]) {
-      dragApp.pullDragName(userFirst, userLast, dragApp.house)
+   if (dragData.firstNames[userFirst] && dragData.lastNames[userLast]) {
+      dragApp.pullDragName(userFirst, userLast, dragData.house)
    } else {
       $('.results').html(errorMessage)
       $('.results').addClass('resultsSlide')
@@ -142,8 +70,8 @@ dragApp.errorCatch = (userFirst, userLast) => {
 // function to pull one drag name based on first initial from the available options
 dragApp.pullDragName = (userFirst, userLast) => {
    // using the array of potential drag names (matched to the users intials), find a random index within that array and save to the variable
-   const dragFirst = dragApp.randomIndex(dragApp.firstNames[userFirst])
-   const dragLast = dragApp.randomIndex(dragApp.lastNames[userLast])
+   const dragFirst = dragApp.randomIndex(dragData.firstNames[userFirst])
+   const dragLast = dragApp.randomIndex(dragData.lastNames[userLast])
    const dragHouse = dragApp.pullDragHouse()
    
    // display the results of the random drag name 
@@ -155,7 +83,7 @@ dragApp.selectedHouse = []
 dragApp.pullDragHouse = () => {
    // checks to see if a random house has already been stored. if not: store a random house. if one is already stored, use the stored house name instead
    if (dragApp.selectedHouse.length === 0) {
-      dragApp.selectedHouse.push(dragApp.randomIndex(dragApp.house))
+      dragApp.selectedHouse.push(dragApp.randomIndex(dragData.house))
       return dragApp.selectedHouse;
    } else {
       return dragApp.selectedHouse;
@@ -163,10 +91,13 @@ dragApp.pullDragHouse = () => {
 }
 
 // display the drag name in a string to the user
+
+// NOTE: New Name button not as clear as I thought it would. Random Name or Better Name.
+// NOTE: Go back button should be on the left side, expect it to be there
 dragApp.displayResults = (firstD, lastD, house) => {
    const displayHTML = `
    <div class="displayBox">
-      <p>Ladies and gentleman please welcome to the stage:</p>
+      <p>Ladies and genders please welcome to the stage:</p>
       <h2>${firstD} ${lastD}</h2>
       <p>from the legendary <span class="dragHouse">House of ${house}!</span></p>
       <form action="" class="resultsForm wrapper" name="resultsForm">
@@ -187,5 +118,4 @@ dragApp.init = () => {
 
 $(function(){
    dragApp.init();
-
 })
